02b882f1a89ba959f474e2095dcaf983048390f2
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-02-24 11:32:34 +0000

    Add training ui

---
+++ training/ElancoUI/modal.js
@@ -0,0 +1,7 @@
+function toggleModal() {
+    if (document.querySelector(".modal").style.display == "block") {
+        document.querySelector(".modal").style.display = "none";
+    } else {
+        document.querySelector(".modal").style.display = "block";
+    }
+}
